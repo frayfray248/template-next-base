3284433e3399f47291a61649ad13e606ad4d9a7a
Fraser <nicman248@hotmail.com>
2024-06-20 14:04:29 -0700

    HR

---
+++ src/app/components/shared/Hr.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hr = () => {
+  return (
+    <hr className="my-6 max-w-6xl mx-auto border-neutral-content border-1" />
+  )
+}
+
+export default Hr
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,18 @@ import FeatureCards from '@/app/components/sections/FeatureCards'
 import FeatureCarousel from "./components/sections/FeatureCarousel";
 import FeatureTimeline from "./components/sections/FeatureTimeline";
 import SignupHero from './components/sections/SignupHero';
+import Hr from '@/app/components/shared/Hr';
 
 export default function Home() {
     return (
         <main className="flex-1">
             <LandingHero />
             <FeatureCards />
+            <Hr />
             <FeatureCarousel />
+            <Hr />
             <FeatureTimeline />
+            <Hr />
             <SignupHero />
         </main>
     );
