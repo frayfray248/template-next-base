c9662998b5755eb483ef2241a5e8a1ee08be34be
Fraser <nicman248@hotmail.com>
2024-06-21 15:02:33 -0700

    hero variants

---
+++ src/app/components/sections/heroes/HeroContact.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroContact = () => {
+    return (
+        <section className="hero min-h-screen">
+            <article className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Contact us!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <a href="mailto:" className="btn btn-primary">Email us</a>
+                </div>
+            </article>
+        </section>
+    )
+}
+
+export default HeroContact
---
+++ src/app/components/sections/heroes/HeroFigure.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeroFigure = ({ reverse } : { reverse : boolean}) => {
+    return (
+        <section className="hero min-h-screen bg-base-200">
+            <article className={`hero-content max-w-4xl flex-col ${reverse ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
+                <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </article>
+        </section>
+    )
+}
+
+export default HeroFigure
