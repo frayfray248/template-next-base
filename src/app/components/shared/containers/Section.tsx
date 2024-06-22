838344cb2cda8adb0e32f2be56096f54195d7030
Fraser <nicman248@hotmail.com>
2024-06-22 11:47:22 -0700

    Section widthes

--- src/app/components/shared/containers/Section.tsx
+++ src/app/components/shared/containers/Section.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Section = ({ children }: { children: React.ReactNode }) => {
+const Section = ({ children, width='lg' }: { children: React.ReactNode, width? : 'screen' | 'lg' | 'md' | 'sm' }) => {
+
+    const widthClass = {
+        'screen': 'max-w-screen',
+        'lg': 'max-w-6xl',
+        'md': 'max-w-4xl',
+        'sm': 'max-w-xl'
+    }
+
     return (
-        <section className='max-w-6xl min-h-screen mx-auto flex flex-col justify-center items-center gap-16 p-6 relative'>
+        <section className={`${widthClass[width]} min-h-screen mx-auto flex flex-col justify-center items-center gap-16 p-6 relative`}>
             {children}
         </section>
     )
