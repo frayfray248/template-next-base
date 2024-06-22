811fb0646e758f4e7028eac3dd3ff431e69c1949
Fraser <nicman248@hotmail.com>
2024-06-22 10:16:39 -0700

    Thumbnail

--- src/app/components/sections/Portfolio.tsx
+++ src/app/components/sections/Portfolio.tsx
@@ -4,8 +4,7 @@ import Section from '@/app/components/shared/containers/Section'
 import Heading from '@/app/components/shared/text/Heading'
 import Paragraph from '@/app/components/shared/text/Paragraph'
 import Row from '@/app/components/shared/containers/Row'
-import Card, { Content, Title } from '@/app/components/shared/Card'
-import { type } from 'os'
+import Thumbnail from '@/app/components/shared/Thumbnail'
 
 const Portfolio = () => {
     return (
@@ -16,8 +15,18 @@ const Portfolio = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec consequat ultricies, nunc purus ultricies nunc, nec ultricies nunc purus nec nunc.
                 </Paragraph>
                 <Row>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
+                    <Thumbnail src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" href="#"/>
 
-                    <Card.Card>
+                    {/* <Card.Card>
                         <Card.Image src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
                         <Card.Content>
                             <Card.Title>Card Title</Card.Title>
@@ -79,8 +88,7 @@ const Portfolio = () => {
                             <Card.Title>Card Title</Card.Title>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         </Card.Content>
-                    </Card.Card>
-
+                    </Card.Card> */}
                 </Row>
             </article>
         </Section>
---
+++ src/app/components/shared/Thumbnail.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+import React from 'react'
+
+const Thumbnail = ({ src, alt, href }: { src: string, alt: string, href: string }) => {
+    return (
+        <a className='relative' href={href} target="_blank" rel="noopener noreferrer">
+            <img className="object-cover w-80 h-80 rounded-2xl " src={src} alt={alt} />
+            <div className='w-full h-full absolute top-0 left-0 rounded-2xl bg-gradient-to-t from-base-200 opacity-0 hover:opacity-100 transition'>
+                <div className='flex justify-center items-center w-full h-full'>
+                    <p className='text-white text-2xl font-bold'>View Project</p>
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default Thumbnail
